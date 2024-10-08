b4c49657f305f28250cf04fd3ab1fb667fb5eb51
Eduardo da Silva <139717098+eduardodsxavier@users.noreply.github.com>
2024-10-08 10:45:57 -0300

    Add files via upload

---
+++ minha-aplicacao-web/src/main/java/com/seuprojeto/minhaaplicacaoweb/HomeController.java
@@ -0,0 +1,13 @@
+package com.seuprojeto.minhaaplicacaoweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
